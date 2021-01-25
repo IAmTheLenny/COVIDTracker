1725ad5cb418de209aac9f1f524510d42a30264a
IAmTheLenny <77951925+IAmTheLenny@users.noreply.github.com>
2021-01-24 20:56:31 -0800

    Add files via upload

---
+++ COVID.py
@@ -0,0 +1,64 @@
+import re
+from datetime import datetime
+try:
+	from bs4 import BeautifulSoup
+except ModuleNotFoundError:
+	print("You do not have the BeautifulSoup module, which is required to run this program.")
+	print("You can download BeautifulSoup from https://www.crummy.com/software/BeautifulSoup/ or running the command 'pip3 install beautifulsoup4' in your command line")
+	exit()
+try:
+	import requests
+except ModuleNotFoundError:
+	print("You do not have the requests module. Please download it before running this program.")
+	print("You can download requests by running the command 'pip3 install requests' in your command line")
+	exit()
+
+#Set up to scrape
+url = "https://ncov2019.live/"
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+#Order to print the results in
+order = ["cases", "recovered", "deaths", "active", "new cases today", "new deaths today"]
+
+#Scrapes all data
+scrapedData = soup.find_all(type="text/javascript")[5]
+scrapedData = str(scrapedData)
+
+#RegEx to find specific results
+worldResults = re.findall('"totals":{"confirmed":([0-9]+?),"recovered":([0-9]+?),"deaths":([0-9]+?),.+?"active":([0-9]+?),"daily_confirmed":([0-9]+?),.+?"daily_deaths":([0-9]+?),', scrapedData)
+worldResults = worldResults[0]
+worldResults = list(worldResults)
+
+USResults = re.findall('"country_code":"us","country":"United States","confirmed":([0-9]+?),"daily_confirmed":([0-9]+?),"recovered":([0-9]+?),.+?,"deaths":([0-9]+?),"daily_deaths":([0-9]+?),.+?,"active":([0-9]+?),', scrapedData)
+USResults = USResults[0]
+USResults = list(USResults)
+
+#Reorders US Results to be the same as World Results
+reorder = [0, 2, 3, 5, 1, 4]
+USResults = [USResults[newOrder] for newOrder in reorder]
+
+#Figures out time and when the COVID data was last updated
+lastUpdated = re.findall('{"last_updated":"([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9])T([0-9][0-9]:[0-9][0-9]:[0-9][0-9])', scrapedData)
+lastUpdated = lastUpdated[0][0] + " " + lastUpdated[0][1]
+currentTime = datetime.utcnow()
+currentTime = str(currentTime)
+currentTime = currentTime[:19]
+timeFormat = '%Y-%m-%d %H:%M:%S'
+timeSinceUpdate = datetime.strptime(currentTime, timeFormat) - datetime.strptime(lastUpdated, timeFormat)
+timeSinceUpdate = str(timeSinceUpdate)
+timeSinceUpdate = timeSinceUpdate.split(":")
+timeSinceUpdate = int(timeSinceUpdate[0]) * 60 + int(timeSinceUpdate[1])
+
+#Prints the data
+def printData(resultName, place):
+	print()
+	for numOfCases in range(len(order)):
+		resultName[numOfCases] = "{:,}".format(int(resultName[numOfCases]))
+		if resultName[numOfCases] == "0":
+			resultName[numOfCases] = "0 or unknown"
+		print(f"{place} {order[numOfCases]}: {resultName[numOfCases]}")	
+
+print(f"Last updated {timeSinceUpdate} minutes ago")
+printData(worldResults, "World")
+printData(USResults, "US")
